92260bd9c456c299e1bcacdfaf291d96b1d14260
YanLiUK <57985834+YanLiUK@users.noreply.github.com>
2023-06-29 18:47:31 +0800

    Update QRISK3_2019_test.R
    update to 0.5

--- R/QRISK3_2019_test.R
+++ R/QRISK3_2019_test.R
@@ -1,6 +1,6 @@
 #' Test data for QRISK3 2017 algorithm - 2019 data
 #'
-#' Data from QRISK3 original algorithm (C code) in 2019 <https://qrisk.org/three/src.php>.
+#' Data from QRISK3 original algorithm (C code) in 2019.
 #' The aim is to compare whether this package calculates the same score as the original algorithm.
 #' "QRISK_C_algorithm_score" in dataset is the score calculated using original algorithm in 2019. 
 #' It should give the same score as this package.
